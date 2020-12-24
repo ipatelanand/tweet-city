4534cc7c147aaf039bb2ba87be053cf1e3f70e94
Anand Patel <ipatelanand@gmail.com>
2020-12-23 19:25:17 -0500

    added indiv page w/ tweets

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const Tweet = require("../models/tweets")
+const User = require("../models/users")
 const home = express.Router()
 
 const isAuthenticated = (req, res, next) => {
@@ -10,9 +11,29 @@ const isAuthenticated = (req, res, next) => {
 	}
 }
 
+const findTweetsByUser = (id, page, req, res) => {
+	User.findById(id, (err, userId) => {
+		Tweet.find({ username: userId.username }, (err, allTweetsFromUser) => {
+			res.render(page, {
+				view_user: userId,
+				tweets: allTweetsFromUser,
+			})
+		})
+	})
+}
+
 home.get("/", (req, res) => {
 	res.render("home.ejs", {
 		currentUser: req.session.currentUser,
+	})
+})
+
+home.get("/allusers", (req, res) => {
+	User.find({}, (err, data) => {
+		res.render("users/show_all.ejs", {
+			currentUser: req.session.currentUser,
+			data: data,
+		})
 	})
 })
 
@@ -20,6 +41,10 @@ home.get("/new", isAuthenticated, (req, res) => {
 	res.render("tweets/new.ejs", {
 		currentUser: req.session.currentUser,
 	})
+})
+
+home.get("/user/show/:id", (req, res) => {
+	findTweetsByUser(req.params.id, "users/indiv_page.ejs", req, res)
 })
 
 home.post("/", isAuthenticated, (req, res) => {
