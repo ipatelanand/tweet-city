2939d64bc77b03a48601f3856090a41fb292655a
Anand Patel <ipatelanand@gmail.com>
2020-12-25 17:58:55 -0500

    updated user schema

--- models/users.js
+++ models/users.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema
 const userSchema = Schema({
 	username: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
+	biography: { type: String },
+	backgroundimg:{type: String} ,
 	img: { type: String },
 	followers: { type: Array },
 	following: { type: Array },
