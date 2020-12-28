5b356ebd834a47b636d867afddcdbb9e0e518da5
Anand Patel <ipatelanand@gmail.com>
2020-12-28 15:18:06 -0500

    lots of updates

--- models/tweets.js
+++ models/tweets.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const User = require("./users.js")
+
 
 const tweetSchema = Schema(
 	{
 		username: { type: String, required: true },
-		title: { type: String, required: true },
+		title: { type: String },
 		body: { type: String, required: true },
 		img: { type: String },
 		privacy: Boolean,
